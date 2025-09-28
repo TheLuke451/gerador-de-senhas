7d12663409ea6510d030c0155096eb58520b6e42
TheLuke <ricardofernandesdearaujo3@hotmail.com>
2025-09-28 14:19:24 -0300

    Segundo Commit: Perguntas ao usuário

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 # Bibliotecas
 import secrets
+
+
+# Perguntas ao usuário
+tamanho_senha = input(int("Digite o tamanho da senha: "))
+caracteres_especiais = input("Incluir caracteres especiais? (s/n): ")
+numeros = input("Incluir números? (s/n): ")
+maiusculas = input("Incluir letras maiúsculas? (s/n): ")
+minusculas = input("Incluir letras minúsculas? (s/n): ")
+
+# Gerar senha para o usuário
+caracteres = ""
+
+if caracteres_especiais == 's' or caracteres_especiais == 'S':
+    caracteres += "!@#$%¨&*()_+{}[];:,.<>?/|"
