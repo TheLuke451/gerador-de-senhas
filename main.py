2da993bb0597a15a8fbf983a4599a3f302bfcfd3
TheLuke <ricardofernandesdearaujo3@hotmail.com>
2025-09-28 14:23:49 -0300

    Terceiro Commit: Geração de senha adicionada

--- main.py
+++ main.py
@@ -8,3 +8,18 @@ caracteres_especiais = input("Incluir caracteres especiais? (s/n): ")
 numeros = input("Incluir números? (s/n): ")
 maiusculas = input("Incluir letras maiúsculas? (s/n): ")
 minusculas = input("Incluir letras minúsculas? (s/n): ")
+
+# Geração da senha
+caracteres = ""
+
+if caracteres_especiais == 's' or caracteres_especiais == 'S':
+    caracteres += "!@#$%^&*()-_=+[]{}|;:,.<>?/"
+if numeros == 's' or numeros == 'S':
+    caracteres += "0123456789"
+if maiusculas == 's' or maiusculas == 'S':
+    caracteres += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+if minusculas == 's' or minusculas == 'S':
+    caracteres += "abcdefghijklmnopqrstuvwxyz"
+
+senha = "".join(secrets.choice(caracteres) for i in range(tamanho_senha))
+print("Senha gerada:", senha)
