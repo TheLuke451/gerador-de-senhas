0df35775991627548f3003d9dc66aa05cde9d2fa
TheLuke <ricardofernandesdearaujo3@hotmail.com>
2025-09-28 14:20:34 -0300

    Segundo Commit: Perguntas ao usuário

--- main.py
+++ main.py
@@ -8,9 +8,3 @@ caracteres_especiais = input("Incluir caracteres especiais? (s/n): ")
 numeros = input("Incluir números? (s/n): ")
 maiusculas = input("Incluir letras maiúsculas? (s/n): ")
 minusculas = input("Incluir letras minúsculas? (s/n): ")
-
-# Gerar senha para o usuário
-caracteres = ""
-
-if caracteres_especiais == 's' or caracteres_especiais == 'S':
-    caracteres += "!@#$%¨&*()_+{}[];:,.<>?/|"
